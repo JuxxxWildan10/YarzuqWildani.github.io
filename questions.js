d0a9b1256f634d673f2c36bb9b41ede700f5d474
JuxxxWildan10 <yarzuqwildani168@gmail.com>
2024-11-26 21:24:40 +0700

    first commit

---
+++ questions.js
@@ -0,0 +1,57 @@
+let questions = [
+    {
+        numb: 1,
+        question: "Apa Kepanjangan Dari HTML?",
+        answer: "C. Hyper Text Markup Language",
+        options: [
+            "A. Hyper Text Multi Language",
+            "B. Hyper Text Multiple Language",
+            "C. Hyper Text Markup Language",
+            "D. Home Text Multi Language",
+        ]
+    },
+    {
+        numb: 2,
+        question: "Apa Kepanjangan Dari CSS?",
+        answer: "A. Cascading Style Sheet",
+        options: [
+            "A. Cascading Style Sheet",
+            "B. Cute Style Sheet",
+            "C. Computer Style Sheet",
+            "D. Code Style Sheet",
+        ]
+    },
+    {
+        numb: 3,
+        question: "Apa Kepanjangan Dari PHP?",
+        answer: "A. Hypertext Preprocessor",
+        options: [
+            "A. Hypertext Preprocessor",
+            "B. Hometext Programming",
+            "C. Hypertext Preprogramming",
+            "D. Programming Hypertext Preprocessor",
+        ]
+    },
+    {
+        numb: 4,
+        question: "Apa Kepanjangan Dari SQL?",
+        answer: "D. Structured Query Language",
+        options: [
+            "A. Strength Query Language",
+            "B. Stylesheet Query Language",
+            "C. Science Query Language",
+            "D. Structured Query Language",
+        ]
+    },
+    {
+        numb: 5,
+        question: "Apa Kepanjangan Dari XML?",
+        answer: "D. Exstensible Markup Language",
+        options: [
+            "A. Excellent Multiple Language",
+            "B. Explore Multiple Language",
+            "C. Extra Markup Language",
+            "D. Exstensible Markup Language",
+        ]
+    }
+];
